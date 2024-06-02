0e22a07a4220dc5dd14abb4c08519937a7cb069d
Harshal Kahar <harshal.kahar@esparkbizmail.com>
2024-06-02 22:33:53 +0530

    sentry configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
-
-
+import * as Sentry from '@sentry/react';
 const App = () => {
   return (
     <main className="bg-black">
-      <Navbar/>
-      <Hero/>
-      <Highlights/>
-      <Model/>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App); 
